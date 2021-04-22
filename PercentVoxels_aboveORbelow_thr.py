5f6be6c553a593d9ec0f9de0d58d170985de7660
ckorponay <64859234+ckorponay@users.noreply.github.com>
2021-04-22 15:41:17 -0400

    Delete PercentVoxels_aboveORbelow_thr.py

--- PercentVoxels_aboveORbelow_thr.py
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-RvL_ManhattanDistance.nii -uthr 0.47 RvL_ManhattanDistance_uTHR.nii
-
-3dBrickStat -count -non-zero RvL_ManhattanDistance_uTHR.nii.gz
-
-3dBrickStat -count -non-zero RvL_ManhattanDistance.nii
